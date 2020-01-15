62bfeb97a5f53b0693057a2457f838bd8bf33aeb
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-01-15 14:47:59 -0500

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Ashraf Khan on 1/15/20.
+//  Copyright © 2020 Ashraf Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
