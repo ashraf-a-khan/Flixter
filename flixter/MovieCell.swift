b8ef5b0b759fb14cd80cecd1af3b3853930b7c93
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-01-18 22:14:13 -0500

    Adds pod files

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Ashraf Khan on 1/18/20.
+//  Copyright © 2020 Ashraf Khan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var synopsisLabel: UILabel!
+    @IBOutlet var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
